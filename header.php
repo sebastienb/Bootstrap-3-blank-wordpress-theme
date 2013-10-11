4ae64b5b3fb2eb2987f88bc399844cfb17ab57e1
Sebastien <sebastien@3sparks.net>
2013-10-10 20:30:48 -0400

    replaced old bs2 tags in html

--- header.php
+++ header.php
@@ -20,10 +20,10 @@
   <body <?php body_class($class); ?>>
     
     <div id="main-container" class="container">
-    <div class="row-fluid">
+    <div class="row">
     
-   <div class="span3"> <a href="<?php bloginfo('url'); ?>"><img src="http://placehold.it/150x90$text=logo" alt="Logo" /></a></div>
-   <div class="span9"><?php wp_nav_menu( array('menu' => 'Top Menu', 'menu_class' => 'nav nav-pills pull-right', )); ?></div>
+   <div class="col-md-3"> <a href="<?php bloginfo('url'); ?>"><img src="http://placehold.it/150x90$text=logo" alt="Logo" /></a></div>
+   <div class="col-md-9"><?php wp_nav_menu( array('menu' => 'Top Menu', 'menu_class' => 'nav nav-pills pull-right', )); ?></div>
     
    </div>
     
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <div class="row">
 	
-	<div class="col-8">
+	<div class="col-md-8">
 	
 		<?php if(have_posts()) : ?>
 		   <?php while(have_posts()) : the_post(); ?>
@@ -24,7 +24,7 @@
 	
 	</div>
 
-	<div class="col-4">
+	<div class="col-md-4">
 	
 	<?php												
 	 if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar')) : //  Sidebar name
