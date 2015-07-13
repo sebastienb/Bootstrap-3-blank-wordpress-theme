d0593a8ed870ad60d45589cc65de9ebcf96b6972
root <hinnerk@gnutzmann.info>
2015-07-13 19:00:03 +0200

    don't hardcode blog name in header

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="<?php echo home_url(); ?>">Dev Site</a>
+        <a class="navbar-brand" href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>
       </div>
    
       <!-- Collect the nav links, forms, and other content for toggling -->
