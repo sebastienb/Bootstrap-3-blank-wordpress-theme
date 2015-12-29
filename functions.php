487a1cd2710b81383268e5c93f42204cc841a8b7
Nathan Beers <nbeers22@gmail.com>
2015-12-29 15:56:58 -0500

    Added theme options support for social and company vision, mission, etc

--- functions.php
+++ functions.php
@@ -153,6 +153,257 @@ if ( ! function_exists( 'bootstrap_setup' ) ):
 endif;
 
 
+// START THEME OPTIONS
+// custom theme options for user in admin area - Appearance > Theme Options
+function pu_theme_menu()
+{
+  add_theme_page( 'Theme Option', 'Theme Options', 'manage_options', 'pu_theme_options.php', 'pu_theme_page');  
+}
+add_action('admin_menu', 'pu_theme_menu');
+
+function pu_theme_page()
+{
+?>
+    <div class="section panel">
+      <h1>Custom Theme Options</h1>
+      <form method="post" enctype="multipart/form-data" action="options.php">
+      <hr>
+        <?php 
+
+          settings_fields('pu_theme_options'); 
+        
+          do_settings_sections('pu_theme_options.php');
+          echo '<hr>';
+        ?>
+            <p class="submit">  
+                <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />  
+            </p>
+      </form>
+    </div>
+    <?php
+}
+
+add_action( 'admin_init', 'pu_register_settings' );
+
+/**
+ * Function to register the settings
+ */
+function pu_register_settings()
+{
+    // Register the settings with Validation callback
+    register_setting( 'pu_theme_options', 'pu_theme_options' );
+
+    // Add settings section
+    add_settings_section( 'pu_text_section', 'Social Links', 'pu_display_section', 'pu_theme_options.php' );
+
+    // Create textbox field
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'twitter_link',
+      'name'      => 'twitter_link',
+      'desc'      => 'Twitter Link - Example: http://twitter.com/username',
+      'std'       => '',
+      'label_for' => 'twitter_link',
+      'class'     => 'css_class'
+    );
+
+    // Add twitter field
+    add_settings_field( 'twitter_link', 'Twitter', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );   
+
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'facebook_link',
+      'name'      => 'facebook_link',
+      'desc'      => 'Facebook Link - Example: http://facebook.com/username',
+      'std'       => '',
+      'label_for' => 'facebook_link',
+      'class'     => 'css_class'
+    );
+
+    // Add facebook field
+    add_settings_field( 'facebook_link', 'Facebook', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );
+
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'gplus_link',
+      'name'      => 'gplus_link',
+      'desc'      => 'Google+ Link - Example: http://plus.google.com/user_id',
+      'std'       => '',
+      'label_for' => 'gplus_link',
+      'class'     => 'css_class'
+    );
+
+    // Add Google+ field
+    add_settings_field( 'gplus_link', 'Google+', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );
+
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'youtube_link',
+      'name'      => 'youtube_link',
+      'desc'      => 'Youtube Link - Example: https://www.youtube.com/channel/channel_id',
+      'std'       => '',
+      'label_for' => 'youtube_link',
+      'class'     => 'css_class'
+    );
+
+    // Add youtube field
+    add_settings_field( 'youtube_ink', 'Youtube', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );
+
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'linkedin_link',
+      'name'      => 'linkedin_link',
+      'desc'      => 'LinkedIn Link - Example: http://linkedin.com/in/username',
+      'std'       => '',
+      'label_for' => 'linkedin_link',
+      'class'     => 'css_class'
+    );
+
+    // Add LinkedIn field
+    add_settings_field( 'linkedin_link', 'LinkedIn', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );
+
+    // Add settings section
+    add_settings_section( 'about_us_section', 'About Us', 'pu_display_section', 'pu_theme_options.php' );
+
+
+    // Company Mission (who we are, what we do, how we do it)
+    // Create textarea field
+    $field_args = array(
+      'type'      => 'textarea',
+      'id'        => 'who_we_are',
+      'name'      => 'who_we_are',
+      'desc'      => 'Who We Are',
+      'std'       => '',
+      'label_for' => 'who_we_are'
+    );
+
+    add_settings_field( 'who_we_are', 'Who We Are', 'pu_display_setting', 'pu_theme_options.php', 'about_us_section', $field_args );   
+
+    // Create textarea field
+    $field_args = array(
+      'type'      => 'textarea',
+      'id'        => 'what_we_do',
+      'name'      => 'what_we_do',
+      'desc'      => 'What We Do',
+      'std'       => '',
+      'label_for' => 'what_we_do'
+    );
+
+    add_settings_field( 'what_we_do', 'What We Do', 'pu_display_setting', 'pu_theme_options.php', 'about_us_section', $field_args );   
+    
+    // Create textarea field
+    $field_args = array(
+      'type'      => 'textarea',
+      'id'        => 'how_we_do_it',
+      'name'      => 'how_we_do_it',
+      'desc'      => 'How We Do It',
+      'std'       => '',
+      'label_for' => 'how_we_do_it'
+    );
+
+    add_settings_field( 'how_we_do_it', 'How We Do It', 'pu_display_setting', 'pu_theme_options.php', 'about_us_section', $field_args );   
+
+
+
+    // Company Vision and Mission
+    add_settings_section( 'vision_and_mission', 'Vision and Mission', 'pu_display_section', 'pu_theme_options.php' );
+
+    // Create textarea field
+    $field_args = array(
+      'type'      => 'textarea',
+      'id'        => 'vision',
+      'name'      => 'vision',
+      'desc'      => 'Vision',
+      'std'       => '',
+      'label_for' => 'vision'
+    );
+
+    add_settings_field( 'vision', 'Vision', 'pu_display_setting', 'pu_theme_options.php', 'vision_and_mission', $field_args );
+
+    // Create textarea field
+    $field_args = array(
+      'type'      => 'textarea',
+      'id'        => 'mission',
+      'name'      => 'mission',
+      'desc'      => 'Mission',
+      'std'       => '',
+      'label_for' => 'mission'
+    );
+
+    add_settings_field( 'mission', 'Mission', 'pu_display_setting', 'pu_theme_options.php', 'vision_and_mission', $field_args );
+
+}
+
+
+// allow wordpress post editor functions to be used in theme options
+function pu_display_setting($args)
+{
+    extract( $args );
+
+    $option_name = 'pu_theme_options';
+
+    $options = get_option( $option_name );
+
+    switch ( $type ) {  
+          case 'text':  
+              $options[$id] = stripslashes($options[$id]);  
+              $options[$id] = esc_attr( $options[$id]);  
+              echo "<input class='regular-text$class' type='text' id='$id' name='" . $option_name . "[$id]' value='$options[$id]' />";  
+              echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
+          break;
+          case 'textarea':  
+              $options[$id] = stripslashes($options[$id]);  
+              //$options[$id] = esc_attr( $options[$id]);
+              $options[$id] = esc_html( $options[$id]); 
+
+              printf(
+              	wp_editor($options[$id], $id, 
+              		array('textarea_name' => $option_name . "[$id]",
+              			'style' => 'width: 200px'
+              			)) 
+				);
+              // echo "<textarea id='$id' name='" . $option_name . "[$id]' rows='10' cols='50'>".$options[$id]."</textarea>";  
+              // echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";  
+          break; 
+    }
+}
+
+function pu_validate_settings($input)
+{
+  foreach($input as $k => $v)
+  {
+    $newinput[$k] = trim($v);
+    
+    // Check the input is a letter or a number
+    if(!preg_match('/^[A-Z0-9 _]*$/i', $v)) {
+      $newinput[$k] = '';
+    }
+  }
+
+  return $newinput;
+}
+
+// Add custom styles to theme options area
+add_action('admin_head', 'custom_style');
+
+function custom_style() {
+  echo '<style>
+    .appearance_page_pu_theme_options .wp-editor-wrap {
+      width: 75%;
+    }
+    .regular-textcss_class {
+    	width: 50%;
+    }
+    .appearance_page_pu_theme_options h3 {
+    	font-size: 2em;
+    	padding-top: 40px;
+    }
+  </style>';
+}
+
+// END THEME OPTIONS
+
+
 /**
  * Load site scripts.
  */
